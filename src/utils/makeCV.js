8d0cb8ff3dc4fde9e99ebfe2947b704002781cfb
Aidan Dunlop <aidan.dunlop@sky.uk>
2020-05-29 23:53:17 +0100

    rename cv pdf file for nicer slug

--- src/utils/makeCV.js
+++ src/utils/makeCV.js
@@ -38,7 +38,7 @@ async function createPDF(outputFile, port = 9000) {
 }
 
 function makeCV(port) {
-  createPDF('public/aidan_dunlop_cv.pdf', port);
+  createPDF('public/cv.pdf', port);
 }
 
 module.exports = { makeCV };
