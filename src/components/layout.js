6e3046a34b5f2cf39e78b620f8e9f84a53ce7785
Aidan Dunlop <aidan.dunlop@sky.uk>
2020-06-01 10:55:48 +0100

    slightly reduce padding

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,9 @@ import { ThemeManagerContext } from 'gatsby-styled-components-dark-mode';
 import Header from './header';
 import { rhythm } from '../utils/typography';
 import { GlobalStyle } from '../utils/theme';
-
+// rh0.7 = 1.092
+// rh1 = 1.56
+// rh1.03 = 1.51
 const GlobalWrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
@@ -13,7 +15,7 @@ const GlobalWrapper = styled.div`
   padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
   
   @media print {
-    padding: ${rhythm(0.7)} ${rhythm(1)};
+    padding: ${rhythm(0.7)} ${rhythm(0.97)};
     max-width: ${rhythm(50)};
   }
 `;
